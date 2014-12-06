3a92be31e7575dca9a6eed3ce731f11b6c595684
cody <cevek@ya.ru>
2014-12-06 22:28:04 +0400

    templ

--- demo4/1.ts
+++ demo4/1.ts
@@ -1,15 +1,30 @@
-class A implements Arg.Component {
+
+class Argy implements Arg.Component {
     render() {
-        return d('div', 'Hello', new B());
+        return d('div',
+            'Hello',
+            23,
+            true,
+            [1, 2, 3],
+            new Booobs(),
+            d.map<number>('div', itemsAtom, item=>item),
+            new Atom(null, null, 'Hooray!'),
+            d.when(trueAtom, ()=>'Is it true')
+        );
     }
 }
 
-class B implements Arg.Component{
+class Booobs implements Arg.Component {
     render() {
         return d('div', 'My name is Arthur');
     }
 }
 
+var trueAtom = new Atom();
+var itemsAtom = new Atom(null, null, [new Booobs(), new Booobs(), new Booobs()]);
+
+
+
 var glob:any = {};
 
-glob.vdom = Arg.publicRender(document.body, new A());
+glob.vdom = Arg.publicRender(document.body, new Argy());
