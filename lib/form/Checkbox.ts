860db24a1e0181b056709518a515a743fd8454f6
cody <cevek@ya.ru>
2014-12-20 22:20:50 +0400

    arg: Checkbox extends FormElement

--- lib/form/Checkbox.ts
+++ lib/form/Checkbox.ts
@@ -1,28 +1,34 @@
 module Arg {
-    export interface ICheckbox<T> extends Arg.Attrs{
-        argModel: Atom<T>;
-        argValue: T;
+    export interface ICheckbox<T> extends IFormElement {
+        model: Atom<T>;
+        value: T;
     }
-    export class Checkbox<T> implements Arg.Component {
+    export class Checkbox<T> extends FormElement implements Component {
         isBlock = false;
-        constructor(private attrs:ICheckbox<T>) {
+        params:ICheckbox<T>;
+
+        constructor(params:ICheckbox<T>, attrs:Attrs = {}) {
+            super(params, attrs);
             this.attrs.type = 'checkbox';
-            this.attrs.onchange = (e:MouseEvent)=>this.onChange(e);
-            this.attrs.checked = ()=>this.attrs.argModel.isEqual(this.attrs.argValue);
+            this.attrs.onchange = (e)=>this.onChange(e);
+            this.attrs.checked = ()=>this.params.model.isEqual(this.params.value);
         }
 
-        private onChange(e:MouseEvent) {
+        private onChange(e:Event) {
             var target = <HTMLInputElement>e.target;
             if (target.checked) {
-                this.attrs.argModel.set(this.attrs.argValue);
+                this.params.model.set(this.params.value);
             }
             else {
-                this.attrs.argModel.setNull();
+                this.params.model.setNull();
             }
         }
 
         render() {
-            return Arg.dom('input', this.attrs);
+            return root('',
+                dom('input', this.attrs),
+                this.label()
+            );
         }
     }
 }
