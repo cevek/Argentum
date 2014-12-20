167c4d422f7c7899cae0144e32fd8a430230b8f8
cody <cevek@ya.ru>
2014-12-20 20:21:46 +0400

    arg: Checkbox remove label, refactoring args

--- lib/form/Checkbox.ts
+++ lib/form/Checkbox.ts
@@ -1,37 +1,28 @@
 module Arg {
-    export interface ICheckbox<T> {
-        model: Atom<T>;
-        label: any;
-        value: T;
-        checkboxAttrs?: Arg.Attrs;
-        labelAttrs?: Arg.Attrs;
+    export interface ICheckbox<T> extends Arg.Attrs{
+        argModel: Atom<T>;
+        argValue: T;
     }
     export class Checkbox<T> implements Arg.Component {
         isBlock = false;
-        constructor(private domAttrs:Arg.Attrs, private params:ICheckbox<T>) {
-            this.params.checkboxAttrs = this.params.checkboxAttrs || {};
-            this.params.checkboxAttrs['type'] = 'checkbox';
-            this.params.checkboxAttrs['onchange'] = (e:MouseEvent)=>this.onChange(e);
-            this.params.checkboxAttrs['checked'] = ()=>this.params.model.isEqual(this.params.value);
+        constructor(private attrs:ICheckbox<T>) {
+            this.attrs['type'] = 'checkbox';
+            this.attrs['onchange'] = (e:MouseEvent)=>this.onChange(e);
+            this.attrs['checked'] = ()=>this.attrs.argModel.isEqual(this.attrs.argValue);
         }
 
         private onChange(e:MouseEvent) {
             var target = <HTMLInputElement>e.target;
             if (target.checked) {
-                this.params.model.set(this.params.value);
+                this.attrs.argModel.set(this.attrs.argValue);
             }
             else {
-                this.params.model.setNull();
+                this.attrs.argModel.setNull();
             }
         }
 
         render() {
-            return Arg.root('', this.domAttrs,
-                Arg.dom('label',
-                    Arg.dom('input', this.params.checkboxAttrs),
-                    Arg.dom('span', this.params.labelAttrs, this.params.label)
-                )
-            );
+            return Arg.dom('input', this.attrs);
         }
     }
 }
