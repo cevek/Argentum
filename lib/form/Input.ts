36ebf1b9a2477d7cd36762b105212e7f2f1c4e3f
cody <cevek@ya.ru>
2014-12-20 20:22:17 +0400

    arg: Input: refactoring args

--- lib/form/Input.ts
+++ lib/form/Input.ts
@@ -1,23 +1,22 @@
 module Arg {
-    export interface IInput {
-        model: Atom<string>;
+    export interface IInput extends Arg.Attrs{
+        argModel: Atom<string>;
     }
     export class Input implements Arg.Component {
         isBlock = false;
-        constructor(private domAttrs:Arg.Attrs, private params:IInput) {}
+        constructor(private attrs:IInput) {
+            this.attrs['type'] = this.attrs['type'] || 'text';
+            this.attrs['oninput'] = (e:KeyboardEvent)=>this.onInput(e);
+            this.attrs['value'] = this.attrs.argModel;
+        }
 
         onInput(e:KeyboardEvent) {
             var target = <HTMLInputElement>e.target;
-            console.log(target.value);
-
-            this.params.model.set(target.value);
+            this.attrs.argModel.set(target.value);
         }
 
         render() {
-            this.domAttrs['type'] = this.domAttrs['type'] || 'text';
-            this.domAttrs['oninput'] = (e:KeyboardEvent)=>this.onInput(e);
-            this.domAttrs['value'] = this.params.model;
-            return Arg.dom('input', this.domAttrs);
+            return Arg.dom('input', this.attrs);
         }
     }
 }
