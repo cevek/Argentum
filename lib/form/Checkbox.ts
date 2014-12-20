f4b9d4bef3155c85d38e53213ade7aa280079a4f
cody <cevek@ya.ru>
2014-12-20 19:51:24 +0400

    arg: checkbox: first run checked,  checkboxAttrs, labelAttrs

--- lib/form/Checkbox.ts
+++ lib/form/Checkbox.ts
@@ -3,11 +3,19 @@ module Arg {
         model: Atom<T>;
         label: any;
         value: T;
+        checkboxAttrs?: Arg.Attrs;
+        labelAttrs?: Arg.Attrs;
     }
     export class Checkbox<T> implements Arg.Component {
-        constructor(private domAttrs:Arg.Attrs, private params:ICheckbox<T>) {}
+        isBlock = false;
+        constructor(private domAttrs:Arg.Attrs, private params:ICheckbox<T>) {
+            this.params.checkboxAttrs = this.params.checkboxAttrs || {};
+            this.params.checkboxAttrs['type'] = 'checkbox';
+            this.params.checkboxAttrs['onchange'] = (e:MouseEvent)=>this.onChange(e);
+            this.params.checkboxAttrs['checked'] = ()=>this.params.model.isEqual(this.params.value);
+        }
 
-        change(e:MouseEvent) {
+        private onChange(e:MouseEvent) {
             var target = <HTMLInputElement>e.target;
             if (target.checked) {
                 this.params.model.set(this.params.value);
@@ -18,12 +26,10 @@ module Arg {
         }
 
         render() {
-            this.domAttrs['type'] = 'checkbox';
-            this.domAttrs['onchange'] = (e:MouseEvent)=>this.change(e);
-            return Arg.root('',
+            return Arg.root('', this.domAttrs,
                 Arg.dom('label',
-                    Arg.dom('input', this.domAttrs),
-                    Arg.dom('span', this.params.label)
+                    Arg.dom('input', this.params.checkboxAttrs),
+                    Arg.dom('span', this.params.labelAttrs, this.params.label)
                 )
             );
         }
