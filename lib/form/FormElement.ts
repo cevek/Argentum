b50aa93a87f29a1443337a16b3f7b98523dd3eee
cody <cevek@ya.ru>
2014-12-21 14:42:37 +0400

    arg: FormElement small fixes

--- lib/form/FormElement.ts
+++ lib/form/FormElement.ts
@@ -1,13 +1,14 @@
 module Arg {
     export interface IFormElement {
         label: any;
+        labelAttrs?: Attrs;
     }
     export class FormElement {
         params: IFormElement;
         attrs: Attrs;
         constructor(params: IFormElement, attrs: Attrs){
             this.params = params;
-            this.attrs = attrs;
+            this.attrs = attrs || {};
             this.attrs.id = this.attrs.id || Math.random().toString(33).substr(2,3);
         }
         label(){
