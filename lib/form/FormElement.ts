8e1311834be566848aad323b3475eaeeee8854d1
cody <cevek@ya.ru>
2014-12-22 11:32:20 +0400

    arg: FormElement classSet for label

--- lib/form/FormElement.ts
+++ lib/form/FormElement.ts
@@ -4,15 +4,20 @@ module Arg {
         labelAttrs?: Attrs;
     }
     export class FormElement {
-        params: IFormElement;
-        attrs: Attrs;
-        constructor(params: IFormElement, attrs: Attrs){
+        params:IFormElement;
+        attrs:Attrs;
+
+        constructor(params:IFormElement, attrs:Attrs) {
             this.params = params;
             this.attrs = attrs || {};
-            this.attrs.id = this.attrs.id || Math.random().toString(33).substr(2,3);
+            this.attrs.id = this.attrs.id || Math.random().toString(33).substr(2, 3);
         }
-        label(){
-            return dom('label', {htmlFor: this.attrs.id}, this.params.label)
+
+        label() {
+            return dom('label', {
+                htmlFor: this.attrs.id,
+                classSet: {required: this.attrs.required}
+            }, this.params.label)
         }
     }
 }
