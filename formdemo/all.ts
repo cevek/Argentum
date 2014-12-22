df97aa827724ad6d2adcf7009c83e489d6a422f8
cody <cevek@ya.ru>
2014-12-22 11:34:30 +0400

    formdemo: templ

--- formdemo/all.ts
+++ formdemo/all.ts
@@ -6,6 +6,8 @@ class User {
 class TestForm implements Arg.Component {
     input = new Atom(this, {name: 'input', value: 'hey'});
     checkbox = new Atom(this, {name: 'checkbox', value: 'yes'});
+    required = new Atom(this, {name: 'required', value: true});
+    multiple = new Atom(this, {name: 'multiple', value: true});
 
     options = [{name: 'No1'}, {name: 'No2'}, {name: 'No3'}, {name: 'No4'}];
     //select = new Atom<User[]>(this, {name: 'select', value: []});
@@ -17,8 +19,13 @@ class TestForm implements Arg.Component {
         return Arg.root('',
             d('form', {onsubmit: ()=>false},
                 new Arg.Checkbox({label: "Click me", value: 'yes', model: this.checkbox}),
+                new Arg.Checkbox({label: "Multiple", value: true, model: this.multiple}),
+                new Arg.Checkbox({label: "Required", value: {afdsa:324}, model: this.required}),
                 new Arg.FormInput({label: "My Text", model: this.input}, {required: true}),
-                new Arg.FormSelect({label: 'select', modelMultiple: this.select}, {m1ultiple: true, required: true},
+                new Arg.FormSelect({label: 'select', modelMultiple: this.select}, {
+                        multiple: this.multiple,
+                        required: this.required
+                    },
                     d('option', {argDefault: true}, 'None'),
                     d('optgroup', {label: 'Lbl'},
                         ()=>this.checkbox.get() ?
