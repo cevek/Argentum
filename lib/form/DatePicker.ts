76a2166c1e559c10af8f8a4bff70f602fc42be9d
cody <cevek@ya.ru>
2014-12-24 22:50:52 +0400

    arg: DatePicker in progress

--- lib/form/DatePicker.ts
+++ lib/form/DatePicker.ts
@@ -3,14 +3,57 @@ module Arg {
 
     }
     export class DatePicker implements Component {
+        value = new Atom<Date>(this, {name: 'value'});
+        inputTree:TreeItem;
+        formatter = new Atom<string>(this, {
+            name: 'formatter',
+
+            //value: 'whowhow',
+            getter: (oldVal)=> {
+                var val = this.value.get();
+                console.log("formatter", val);
+                if (val && isFinite(val.getTime())) {
+                    if (this.inputTree) {
+                        (<HTMLInputElement>this.inputTree.node).setCustomValidity('');
+                    }
+                    return ('0' + val.getDate()).substr(-2) + '.' + ('0' + (val.getMonth() + 1)).substr(-2) + '.' + val.getFullYear();
+                }
+                if (this.inputTree) {
+                    (<HTMLInputElement>this.inputTree.node).setCustomValidity('Invalid date');
+                }
+                return oldVal;
+            }
+        });
+
         constructor(private params:IDatePicker, private attrs:Attrs = {}) {
 
+        }
+
+        onChange(e:Event) {
+            var input = <HTMLInputElement>e.target;
+            var value = input.value.trim().replace(/[^\d]+/g, '/');
+            value = value.replace(/^(\d{1,2})\/(\d{1,2})\//, '$2/$1/');
+            var date = new Date(value);
+            console.log("onchange", value, date);
+
+            this.formatter.set(input.value);
+            if (value.length > 5 && isFinite(date.getTime()) && date.getFullYear() > 999 && date.getFullYear() < 3000) {
+                this.value.set(date);
+            }
+            else {
+                this.value.setNull();
+            }
         }
 
         render() {
             return root('',
                 new DatePickerCalendar(),
-                dom('input', {type: 'text'})
+                this.inputTree = dom('input', {
+                    type: 'text',
+                    value: this.formatter,
+                    required: true,
+                    oninput: (e:Event)=>this.onChange(e)
+                })
             );
         }
     }
@@ -44,7 +87,7 @@ module Arg {
             this.calc();
         }
 
-        move(pos: number) {
+        move(pos:number) {
 
         }
 
