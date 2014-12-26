b2e604d4bbda90144deda2111dd566774d00a812
cody <cevek@ya.ru>
2014-12-26 22:12:22 +0300

    arg: datepicker in progress

--- lib/form/DatePicker.ts
+++ lib/form/DatePicker.ts
@@ -11,11 +11,11 @@ module Arg {
         parser() {
             var value = this.inputNode.value.trim().replace(/[^\d]+/g, '/');
             value = value.replace(/^(\d{1,2})\/(\d{1,2})\//, '$2/$1/');
-            var has3digitBlocks = value.match(/(\d{1,4})\/(\d{1,2})\/(\d{1,4})/);
+            var has3DigitBlocks = value.match(/(\d{1,4})\/(\d{1,2})\/(\d{1,4})/);
             var date = new Date(value);
             console.log("onchange", value, date);
 
-            if (value.length > 5 && has3digitBlocks && isFinite(date.getTime()) && date.getFullYear() > 999 && date.getFullYear() < 3000) {
+            if (value.length > 5 && has3DigitBlocks && isFinite(date.getTime()) && date.getFullYear() > 999 && date.getFullYear() < 3000) {
                 this.inputNode.setCustomValidity('');
                 this.params.model.set(date);
             }
@@ -36,6 +36,7 @@ module Arg {
         updateInput() {
             if (this.inputNode !== document.activeElement) {
                 this.formatter();
+                this.inputNode.setCustomValidity('');
             }
         }
 
@@ -47,7 +48,7 @@ module Arg {
 
         render() {
             return root('',
-                new DatePickerCalendar(),
+                new DatePickerCalendar(this.params.model),
                 this.inputTree = dom('input', {
                     type: 'text',
                     required: true,
@@ -63,26 +64,28 @@ module Arg {
     }
 
     export class DatePickerCalendar implements Component {
-        private days = new Atom<Date[][]>(this, this.calc);
-        private activeMonth = new Atom<number>(this, new Date().getMonth());
-        private activeYear = new Atom<number>(this, new Date().getFullYear());
-        private currentDay = DatePickerCalendar.getDayInt(new Date());
-        private firstDayOfMonth = new Atom<Date>(this, DatePickerCalendar.getMonday(new Date()));
+        private firstDayOfMonth = new Atom<Date>(this, new Date(new Date().getFullYear(), new Date().getMonth(), 1));
 
+        private currentDay = DatePickerCalendar.getDayInt(new Date());
 
         static getDayInt(date:Date) {
             return date.getFullYear() * 10000 + (date.getMonth() + 1) * 100 + date.getDate();
         }
 
-        static getMonday(date:Date) {
+        static getMonday(dt:Date) {
+            var date = new Date(dt.getTime());
             var weekDay = date.getDay();
             var diff = date.getDate() - weekDay + (weekDay == 0 ? -6 : 1);
             return new Date(date.setDate(diff));
         }
 
+        private days = new Atom<Date[][]>(this, this.calc);
+
         calc() {
             var days:Date[][] = [];
-            var start = DatePickerCalendar.getMonday(new Date(this.activeYear.get(), this.activeMonth.get(), 1));
+            var start = DatePickerCalendar.getMonday(this.firstDayOfMonth.get());
+            console.log(start);
+
             for (var j = 0; j < 42; j++) {
                 var week = j / 7 | 0;
                 days[week] = days[week] || [];
@@ -91,22 +94,28 @@ module Arg {
             return days;
         }
 
-        constructor() {
+        constructor(private model:Atom<Date>) {
         }
 
         move(pos:number) {
+            var dt = this.firstDayOfMonth.get();
+            var nDt = new Date(dt.getTime());
             if (pos === 1 || pos === -1) {
-                this.activeMonth.set(this.activeMonth.get() + pos);
+                nDt.setMonth(dt.getMonth() + pos);
             }
             if (pos === 0) {
-                this.activeMonth.set(new Date().getMonth());
+                nDt = new Date(new Date().getFullYear(), new Date().getMonth(), 1);
             }
-            this.firstDayOfMonth.set(new Date(this.activeYear.get(), this.activeMonth.get(), 1));
-            //this.calc();
+            console.log("move", dt, nDt);
+            this.firstDayOfMonth.set(nDt);
+        }
+
+        click(day: Date){
+            this.model.set(day);
         }
 
         render() {
-            return dom('div.calendar',
+            return root('',
                 dom('.controls',
                     dom('a.left', {onclick: ()=>this.move(-1)}, '<'),
                     dom('a.current', {onclick: ()=>this.move(0)}, '.'),
@@ -117,8 +126,11 @@ module Arg {
                             dom('span.day', {
                                     classSet: {
                                         'current': this.currentDay === DatePickerCalendar.getDayInt(day),
-                                        'current-month': ()=>this.firstDayOfMonth.get().getMonth() === day.getMonth()
-                                    }
+                                        'current-month': ()=>this.firstDayOfMonth.get().getMonth() === day.getMonth(),
+                                        'active': ()=>this.model.get() && DatePickerCalendar.getDayInt(this.model.get()) == DatePickerCalendar.getDayInt(day)
+                                    },
+                                    onclick: ()=>this.click(day)
+
                                 },
                                 day.getDate()))))
             );
