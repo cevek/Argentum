ddc9340f82a97e98fcfe9a962a95fd49ec8c5dad
cody <cevek@ya.ru>
2015-01-13 22:23:33 +0300

    watcher: add timeout

--- watcher/watcher.js
+++ watcher/watcher.js
@@ -4,6 +4,9 @@ var client = net.connect({port: 9090},
 		//console.log('connected to server!');
 		client.write(process.argv[2]);
 	});
+client.setTimeout(5000, function(){
+	client.end();
+});
 client.on('data', function (data) {
 	console.log(data.toString());
 });
