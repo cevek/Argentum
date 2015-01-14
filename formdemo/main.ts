c616dfb3c024f582d09bcca8171ce02c887daa7d
cody <cevek@ya.ru>
2015-01-14 21:58:25 +0300

    formdemo: test new functions

--- formdemo/main.ts
+++ formdemo/main.ts
@@ -1,179 +1,177 @@
 /// <reference path="../lib/Argentum.ts"/>
 //Atom.debugMode = false;
 var glob:any = {};
+module ag.test {
 
-class TestForm implements ag.Component {
-    input = Atom.source(this, 'input');
-    checkbox = Atom.source(this, 'yes');
-    required = Atom.source(this, true);
-    multiple = Atom.source(this, true);
+    class TestForm implements Component {
+        input = Atom.source(this, 'input');
+        checkbox = Atom.source(this, 'yes');
+        required = Atom.source(this, true);
+        multiple = Atom.source(this, true);
 
-    options = [{name: 'No1'}, {name: 'No2'}, {name: 'No3'}, {name: 'No4'}];
-    //select = new Atom<User[]>(this, {name: 'select', value: []});
-    select = Atom.source(this, [this.options[2]]);
-    date = Atom.source(this, new Date());
+        options = [{name: 'No1'}, {name: 'No2'}, {name: 'No3'}, {name: 'No4'}];
+        //select = new Atom<User[]>(this, {name: 'select', value: []});
+        select = Atom.source(this, [this.options[2]]);
+        date = Atom.source(this, new Date());
 
-    picker = new Atom<ag.TreeItem>(this);
+        picker = new Atom<TreeItem>(this);
 
-    componentDidMount() {
-        glob.picker = this.picker.get().component;
-    }
+        componentDidMount() {
+            glob.picker = this.picker.get().component;
+        }
 
+        activeTab = new Atom(this, null, {value: 2});
 
-    activeTab = new Atom(this, null, {value: 2});
-    render() {
-        var condItem = {name: "Condition"};
-        //this.select.set(options);
-        return ag.root('',
-            ag.dom('form', {onsubmit: ()=>false},
-                new ag.DatePicker({model: this.date}, {self: this.picker}),
-                new ag.Checkbox({label: "Click me", value: 'yes', model: this.checkbox}),
-                new ag.Checkbox({label: "Multiple", value: true, model: this.multiple}),
-                new ag.Checkbox({label: "Required", value: true, model: this.required}),
-                new ag.InputGroup({label: "My Text", model: this.input, required: true}),
-                new ag.SelectGroup({
-                        label: 'select',
-                        modelMultiple: this.select,
-                        multiple: this.multiple,
-                        required: this.required
-                    },
-                    null,
-                    ag.dom('option', {argDefault: true}, 'None'),
-                    ag.dom('optgroup', {label: 'Lbl'},
-                        ag.when(this.checkbox, ()=>
-                            ag.dom('option', {argValue: condItem}, condItem.name)),
+        render() {
+            var condItem = {name: "Condition"};
+            //this.select.set(options);
+            return root('',
+                form({onsubmit: ()=>false},
+                    datepicker({model: this.date, attrs: {self: this.picker}}),
+                    checkbox({label: "Click me", value: 'yes', model: this.checkbox}),
+                    checkbox({label: "Multiple", value: true, model: this.multiple}),
+                    checkbox({label: "Required", value: true, model: this.required}),
+                    inputgroup({label: "My Text", model: this.input, required: true}),
+                    selectgroup({
+                            label: 'select',
+                            modelMultiple: this.select,
+                            multiple: this.multiple,
+                            required: this.required
+                        },
+                        null,
+                        option({argDefault: true}, 'None'),
+                        optgroup({label: 'Lbl'},
+                            when(this.checkbox, ()=>
+                                option({argValue: condItem}, condItem.name)),
 
-                        ag.mapRaw(this.options, option =>
-                            ag.dom('option', {argValue: option}, option.name))
+                            mapRaw(this.options, opt =>
+                                option({argValue: opt}, opt.name))
+                        )
+                    ),
+                    map(this.select, item=>div(item.name)),
+                    this.input,
+                    button({type: 'button', onclick: ()=>this.select.set([])}, 'clear'),
+                    button({
+                        type: 'button',
+                        onclick: ()=> {new Dialog({header: 'hey', body: 'boy', footer: 'clickme'})}
+                    }, 'dialog'),
+                    button('Send'),
+                    button({onclick: ()=>this.activeTab.set(1)}, 'Tab1'),
+                    button({onclick: ()=>this.activeTab.set(2)}, 'Tab2'),
+                    button({onclick: ()=>this.activeTab.set(3)}, 'Tab3'),
+                    this.activeTab,
+                    tabs({model: this.activeTab},
+                        tab({title: "Tab 1", value: 1}, 'Content1'),
+                        tab({title: "Tab 2", value: 2, disabled: true}, 'Content2'),
+                        tab({title: "Tab 3", value: 3}, 'Content3')
                     )
-                ),
-                ag.map(this.select, item=>ag.dom('div', item.name)),
-                this.input,
-                ag.dom('button', {type: 'button', onclick: ()=>this.select.set([])}, 'clear'),
-                ag.dom('button', {
-                    type: 'button', onclick: ()=> {
-                        var dialog = new ag.Dialog({header: 'hey', body: 'boy', footer: 'clickme'});
-                        dialog.open();
-
-                    }
-                }, 'dialog'),
-                ag.dom('button', 'Send'),
-                ag.dom('button', {onclick: ()=>this.activeTab.set(1)}, 'Tab1'),
-                ag.dom('button', {onclick: ()=>this.activeTab.set(2)}, 'Tab2'),
-                ag.dom('button', {onclick: ()=>this.activeTab.set(3)}, 'Tab3'),
-                this.activeTab,
-                new ag.Tabs({model: this.activeTab}, {}, [
-                    new ag.Tab({title: "Tab 1", value: 1, content: () => 'Content1'}),
-                    new ag.Tab({title: "Tab 2", value: 2, content: () => 'Content2'}),
-                    new ag.Tab({title: "Tab 3", value: 3, content: () => 'Content3'}),
-                ])
-            )
-            /*
-             this.checkbox,
-             ()=>this.checkbox.get() ? d('div', 'Fuck') : d('div', 'Man'),
-             this.input
-             */
-        );
-    }
-}
-
-
-class Bench implements ag.Component {
-
-    atoms:Atom<number>[] = [];
-
-    constructor() {
-        for (var i = 0; i < 10000; i++) {
-            this.atoms[i] = Atom.source(this, 0);
+                )
+                /*
+                 this.checkbox,
+                 ()=>this.checkbox.get() ? d('div', 'Fuck') : d('div', 'Man'),
+                 this.input
+                 */
+            );
         }
     }
 
-    click() {
-        var start = Math.random() * 1000 | 0;
-        for (var i = 0; i < 10000; i++) {
-            this.atoms[i].set(start + i);
-        }
-        //console.profile('click');
-        console.time('click');
-        setTimeout(function () {
-            console.timeEnd('click');
-            //console.profileEnd('click');
-        });
-    }
+    class Bench implements Component {
 
-    render() {
-        return ag.root('',
-            ag.dom('button', {onclick: ()=>this.click()}, 'click'),
-            ag.dom('ul', {style: {display: 'none'}},
-                ag.mapRaw(this.atoms, (atom, i)=>ag.dom('li', 'item ' + i + ' ', atom))))
-    }
-}
+        atoms:Atom<number>[] = [];
 
-Atom.debugMode = false;
-var testForm = new TestForm();
-var dom = ag.publicRender(document.body, testForm);
-
-ag.Route.listen();
-module R {
-    export var index = new ag.Route('/');
-    export var about = new ag.Route('/about');
-    export var company = new ag.Route('/company');
-    export var users = new ag.Route('/users');
-    export var userInfo = new ag.Route<{user: any}>('/users/:user');
-    export var userProfile = new ag.Route<{user: any}>('/users/:user/profile');
-    export var userProfileContacts = new ag.Route<{user: any}>('/users/:user/profile/contacts');
-    export var userProfileFriends = new ag.Route<{user: any}>('/users/:user/profile/friends');
-    export var userProfileOffers = new ag.Route<{user: any}>('/users/:user/profile/offers');
-    export var userProfileOffersItem = new ag.Route<{user: any; offer: any}>('/users/:user/profile/offers/:offer');
-}
-
-class Index implements ag.Component {
-    render() {
-
-    }
-}
-
-class User implements ag.Component {
-    render() {
-
-    }
-}
-class UserProfile implements ag.Component {
-    render() {
-
-    }
-}
-class Users {
-    page = new Atom<ag.Component>(this);
-
-    constructor() {
-        ag.Route.activeRoute.addListener(route => {
-            switch (route) {
-                case R.userInfo:
-                    this.page.set(new User());
-                    break;
-
-                case R.userProfileOffersItem:
-                    R.userProfileOffersItem.getParams();
-                    this.page.set(new UserProfile());
-                    break;
+        constructor() {
+            for (var i = 0; i < 10000; i++) {
+                this.atoms[i] = Atom.source(this, 0);
             }
-        }, this);
+        }
+
+        click() {
+            var start = Math.random() * 1000 | 0;
+            for (var i = 0; i < 10000; i++) {
+                this.atoms[i].set(start + i);
+            }
+            //console.profile('click');
+            console.time('click');
+            setTimeout(function () {
+                console.timeEnd('click');
+                //console.profileEnd('click');
+            });
+        }
+
+        render() {
+            return root('',
+                button({onclick: ()=>this.click()}, 'click'),
+                ul({style: {display: 'none'}},
+                    mapRaw(this.atoms, (atom, i)=>li('item ' + i + ' ', atom))))
+        }
     }
 
-    render() {
-        return ag.dom('div',
-            new ag.Link({href: R.userProfileOffersItem.toURL({user: 123, offer: 435})}, 'MyOffer 1'),
-            this.page);
+    Atom.debugMode = false;
+    var testForm = new TestForm();
+    var dom = publicRender(document.body, testForm);
+
+    Route.listen();
+    module R {
+        export var index = new Route('/');
+        export var about = new Route('/about');
+        export var company = new Route('/company');
+        export var users = new Route('/users');
+        export var userInfo = new Route<{user: any}>('/users/:user');
+        export var userProfile = new Route<{user: any}>('/users/:user/profile');
+        export var userProfileContacts = new Route<{user: any}>('/users/:user/profile/contacts');
+        export var userProfileFriends = new Route<{user: any}>('/users/:user/profile/friends');
+        export var userProfileOffers = new Route<{user: any}>('/users/:user/profile/offers');
+        export var userProfileOffersItem = new Route<{user: any; offer: any}>('/users/:user/profile/offers/:offer');
     }
+
+    class Index implements Component {
+        render() {
+
+        }
+    }
+
+    class User implements Component {
+        render() {
+
+        }
+    }
+    class UserProfile implements Component {
+        render() {
+
+        }
+    }
+    class Users {
+        page = new Atom<Component>(this);
+
+        constructor() {
+            Route.activeRoute.addListener(route => {
+                switch (route) {
+                    case R.userInfo:
+                        this.page.set(new User());
+                        break;
+
+                    case R.userProfileOffersItem:
+                        R.userProfileOffersItem.getParams();
+                        this.page.set(new UserProfile());
+                        break;
+                }
+            }, this);
+        }
+
+        render() {
+            return div(
+                new Link({href: R.userProfileOffersItem.toURL({user: 123, offer: 435})}, 'MyOffer 1'),
+                this.page);
+        }
+    }
+
+    publicRender(document.body, new Link({href: R.company.toURL({})}, 'Company'));
+    publicRender(document.body, new Users());
 }
-
-ag.publicRender(document.body, new ag.Link({href: R.company.toURL({})}, 'Company'));
-ag.publicRender(document.body, new Users());
 
 /*
  var bench = new Bench();
  console.time('perf');
- var dom = ag.publicRender(document.body, bench);
+ var dom = publicRender(document.body, bench);
  console.timeEnd('perf');
  */
