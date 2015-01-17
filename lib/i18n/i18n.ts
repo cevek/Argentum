a46b67faa1ca127579eeebcea08bdce3e973e1ba
cody <cevek@ya.ru>
2015-01-17 22:29:12 +0300

    ag: plural, i18n full refactoring

--- lib/i18n/i18n.ts
+++ lib/i18n/i18n.ts
@@ -1,23 +1,25 @@
 module ag {
-    enum PLURAL_CATEGORY {ZERO, ONE, TWO, FEW, MANY, OTHER}
-    interface Lang {
-        plural(n:number, opt_precision:number): PLURAL_CATEGORY;
-    }
-    class EN implements Lang {
-        name = "en";
-        plurals = [PLURAL_CATEGORY.ONE, PLURAL_CATEGORY.OTHER];
+    export module _i18n {
+        export enum PLURAL_CATEGORY {ZERO, ONE, TWO, FEW, MANY, OTHER}
+        export interface Lang {
+            id: string;
+            DATETIME_FORMATS: IDATETIME_FORMATS;
+            NUMBER_FORMATS: INUMBER_FORMATS;
+            texts?: any;
+            plural(n:number, opt_precision:number):PLURAL_CATEGORY;
+        }
 
-        getDecimals(nn:number) {
+        export function getDecimals(nn:number) {
             var n = nn + '';
             var i = n.indexOf('.');
             return (i == -1) ? 0 : n.length - i - 1;
         }
 
-        getVF(n:number, opt_precision?:number) {
+        export function getVF(n:number, opt_precision?:number) {
             var v = opt_precision;
 
             if (undefined === v) {
-                v = Math.min(this.getDecimals(n), 3);
+                v = Math.min(getDecimals(n), 3);
             }
 
             var base = Math.pow(10, v);
@@ -25,79 +27,137 @@ module ag {
             return {v: v, f: f};
         }
 
-        plural(n:number, opt_precision?:number) {
-            var i = n | 0;
-            var vf = this.getVF(n, opt_precision);
-            if (i == 1 && vf.v == 0) {
-                return PLURAL_CATEGORY.ONE;
-            }
-            return PLURAL_CATEGORY.OTHER;
+        interface IDATETIME_FORMATS {
+            AMPMS: string[];
+            DAY: string[];
+            MONTH: string[];
+            SHORTDAY: string[];
+            SHORTMONTH: string[];
+            fullDate: string;
+            longDate: string;
+            medium: string;
+            mediumDate: string;
+            mediumTime: string;
+            short: string;
+            shortDate: string;
+            shortTime: string;
         }
-    }
-    var lang = new EN();
 
-    export function i18n(str:string, ...args:any[]) {
-        if (str) {
-            var children:any[] = [];
-            var splits = str.split('{}');
-            for (var i = 0; i < splits.length; i++) {
-                children.push(splits[i]);
-                if (args[i] !== void 0) {
-                    children.push(args[i]);
-                }
-            }
-            console.log("plural", children);
-            return d('i18n', children);
+        interface INUMBER_FORMATS {
+            CURRENCY_SYM: string;
+            DECIMAL_SEP: string;
+            GROUP_SEP: string;
+            PATTERNS: IPATTERNS[];
         }
+
+        interface IPATTERNS {
+            "gSize": number;
+            "lgSize": number;
+            "maxFrac": number;
+            "minFrac": number;
+            "minInt": number;
+            "negPre": string;
+            "negSuf": string;
+            "posPre": string;
+            "posSuf": string;
+        }
+
     }
 
+    /*
+        // ru version
+        export interface IPlural {
+            one: string;
+            few: string;
+            many: string;
+            other: string;
+        }
+    */
     export interface IPlural {
-        one?: string;
-        two?: string;
-        few?: string;
-        many?: string;
-        other?: string;
-        zero?: string;
+        //one?: string;
+        //two?: string;
+        //few?: string;
+        //many?: string;
+        //other?: string;
+        //zero?: string;
         [type: string]: string;
     }
 
-    export function plural(obj:IPlural, count:any, ...args:any[]):any {
+    export var defaultLang = new _i18n.EN();
+    export interface IPlural {
+        one: string;
+        other: string;
+    }
+
+    export var lang = defaultLang;
+
+    export var texts:any = {
+        'Friend': 'Друг',
+        'Hello, my dear {}! Yeah {} man!': 'Привет, мой дорогой {}! Да {} чувак!',
+        'one {} layer': {
+            ru: {
+                0: 'Никого из игроков',
+                one: '{} игрок',
+                few: '{} игрока',
+                many: '{} игроков',
+                other: '{} игроков'
+            }
+        }
+    };
+
+    export function i18n(str:string, ...args:any[]) {
+        var sttr = str;
+        if (defaultLang !== lang) {
+            if (texts && texts[str]) {
+                sttr = texts[str];
+            }
+            else {
+                console.error('i18n:', str);
+            }
+        }
+        var children:any[] = [];
+        var splits = sttr.split('{}');
+        for (var i = 0; i < splits.length; i++) {
+            children.push(splits[i]);
+            if (args[i] !== void 0) {
+                children.push(args[i]);
+            }
+        }
+        return d('i18n', children);
+    }
+
+    export function plural(obj:IPlural, count:any):any {
         if (count instanceof Atom) {
             var atom:Atom<number> = count;
-            return new Atom(atom.owner, () => {
-                return plural.apply(null, [obj, atom.get()].concat(args))
-            }, {name: 'plural'});
+            return new Atom(atom.owner, () => plural(obj, atom.get()), {name: 'plural'});
         }
 
-        var doestExistsCats:string[] = [];
-        for (var i = 0; i < lang.plurals.length; i++) {
-            var pluralCat = PLURAL_CATEGORY[lang.plurals[i]].toLowerCase();
-            if (obj[pluralCat] === void 0) {
-                doestExistsCats.push(pluralCat);
-            }
-        }
-        if (doestExistsCats.length) {
-            console.error("Plural categories doesn't exists:", doestExistsCats.join());
-        }
-
+        var pluralCat:string;
         if (obj[count]) {
-            return i18n.apply(null, [obj[count]].concat(args));
+            pluralCat = count;
         }
         else {
-            switch (lang.plural(count)) {
-                case PLURAL_CATEGORY.ONE:
-                    return i18n.apply(null, [obj.one].concat(args));
-                case PLURAL_CATEGORY.TWO:
-                    return i18n.apply(null, [obj.two].concat(args));
-                case PLURAL_CATEGORY.FEW:
-                    return i18n.apply(null, [obj.few].concat(args));
-                case PLURAL_CATEGORY.MANY:
-                    return i18n.apply(null, [obj.many].concat(args));
-                case PLURAL_CATEGORY.OTHER:
-                    return i18n.apply(null, [obj.other].concat(args));
-                case PLURAL_CATEGORY.ZERO:
-                    return i18n.apply(null, [obj.zero].concat(args));
+            pluralCat = _i18n.PLURAL_CATEGORY[lang.plural(count)].toLowerCase();
+        }
+
+        var str = obj[pluralCat];
+        if (defaultLang !== lang) {
+            if (texts && texts[obj.one] && texts[obj.one][lang.id] && texts[obj.one][lang.id][pluralCat]) {
+                str = texts[obj.one][lang.id][pluralCat];
+            }
+            else {
+                console.error('plural:', obj, pluralCat);
             }
         }
+        var children:any[] = [];
+        var splits = str.split('{}');
+        for (var i = 0; i < splits.length; i++) {
+            children.push(splits[i]);
+            if (i < splits.length - 1) {
+                children.push(count);
+            }
+        }
+        return d('i18nplural', children);
     }
+
 }
